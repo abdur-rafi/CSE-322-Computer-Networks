a95a2cff9a9bef3efb95fe9923ddfad5d7edc2fc
abdur-rafi <abdurrafi403@gmail.com>
2023-02-12 12:09:53 +0600

    back end code added

---
+++ project-ns2/backend/tcpMod.cc
@@ -0,0 +1,487 @@
+#include <stdlib.h>
+#include <math.h>
+#include <sys/types.h>
+#include "ip.h"
+#include "tcp.h"
+#include "flags.h"
+#include "random.h"
+#include "basetrace.h"
+#include "hdr_qs.h"
+
+static class TcpAgentFitTclClass : public TclClass {
+public:
+	TcpAgentFitTclClass() : TclClass("Agent/TCP/FitW") {}
+	TclObject* create(int, const char*const*) {
+		return (new TcpAgentMod());
+	}
+} class_agent_fit;
+
+
+
+TcpAgentMod::TcpAgentMod() : TcpAgent(), N(1), alpha(.5), windowSum(0), rttSum(0), windowUpdateCount(0), rttUpdateCount(0){
+    bind("N", &N);
+	bind("alpha", &alpha);
+    t_rtt_min = __INT_MAX__;
+}
+
+
+void TcpAgentMod::opencwnd(){
+    // printf("from opencwd\n");
+
+    double increment, _;
+	if (cwnd_ < ssthresh_) {
+		/* slow-start (exponential) */
+		cwnd_ += 1;
+	} else {
+		/* linear */
+		double f;
+		// printf("wnd_option:%d\n", wnd_option_);
+		switch (wnd_option_) {
+		case 0:
+			if (++count_ >= cwnd_) {
+				count_ = 0;
+				++cwnd_;
+			}
+			break;
+
+		case 1:
+			/* This is the standard algorithm. */
+			increment = (increase_num_ * N) / cwnd_;
+			if ((last_cwnd_action_ == 0 ||
+			  last_cwnd_action_ == CWND_ACTION_TIMEOUT) 
+			  && max_ssthresh_ > 0) {
+				increment = limited_slow_start(cwnd_,
+				  max_ssthresh_, increment);
+			}
+			cwnd_ += increment;
+			_ = cwnd_;
+			// printf("cwnd : %lf\n", _);
+			break;
+
+		case 2:
+			/* These are window increase algorithms
+			 * for experimental purposes only. */
+			/* This is the Constant-Rate increase algorithm 
+                         *  from the 1991 paper by S. Floyd on "Connections  
+			 *  with Multiple Congested Gateways". 
+			 *  The window is increased by roughly 
+			 *  wnd_const_*RTT^2 packets per round-trip time.  */
+			f = (t_srtt_ >> T_SRTT_BITS) * tcp_tick_;
+			f *= f;
+			f *= wnd_const_;
+			/* f = wnd_const_ * RTT^2 */
+			f += fcnt_;
+			if (f > cwnd_) {
+				fcnt_ = 0;
+				++cwnd_;
+			} else
+				fcnt_ = f;
+			break;
+
+		case 3:
+			/* The window is increased by roughly 
+			 *  awnd_^2 * wnd_const_ packets per RTT,
+			 *  for awnd_ the average congestion window. */
+			f = awnd_;
+			f *= f;
+			f *= wnd_const_;
+			f += fcnt_;
+			if (f > cwnd_) {
+				fcnt_ = 0;
+				++cwnd_;
+			} else
+				fcnt_ = f;
+			break;
+
+                case 4:
+			/* The window is increased by roughly 
+			 *  awnd_ * wnd_const_ packets per RTT,
+			 *  for awnd_ the average congestion window. */
+                        f = awnd_;
+                        f *= wnd_const_;
+                        f += fcnt_;
+                        if (f > cwnd_) {
+                                fcnt_ = 0;
+                                ++cwnd_;
+                        } else
+                                fcnt_ = f;
+                        break;
+		case 5:
+			/* The window is increased by roughly wnd_const_*RTT 
+			 *  packets per round-trip time, as discussed in
+			 *  the 1992 paper by S. Floyd on "On Traffic 
+			 *  Phase Effects in Packet-Switched Gateways". */
+                        f = (t_srtt_ >> T_SRTT_BITS) * tcp_tick_;
+                        f *= wnd_const_;
+                        f += fcnt_;
+                        if (f > cwnd_) {
+                                fcnt_ = 0;
+                                ++cwnd_;
+                        } else
+                                fcnt_ = f;
+                        break;
+                case 6:
+                        /* binomial controls */ 
+                        cwnd_ += increase_num_ / (cwnd_*pow(cwnd_,k_parameter_));                
+                        break; 
+ 		case 8: 
+			/* high-speed TCP, RFC 3649 */
+			increment = increase_param();
+			if ((last_cwnd_action_ == 0 ||
+			  last_cwnd_action_ == CWND_ACTION_TIMEOUT) 
+			  && max_ssthresh_ > 0) {
+				increment = limited_slow_start(cwnd_,
+				  max_ssthresh_, increment);
+			}
+			cwnd_ += increment;
+                        break;
+		default:
+#ifdef notdef
+			/*XXX*/
+			error("illegal window option %d", wnd_option_);
+#endif
+			abort();
+		}
+	}
+	// if maxcwnd_ is set (nonzero), make it the cwnd limit
+	if (maxcwnd_ && (int(cwnd_) > maxcwnd_))
+		cwnd_ = maxcwnd_;
+
+	// printf("cwnd : %lf avgcwnd : %lf\n",cwnd_.getValue(), awnd_);
+
+	return;
+}
+void TcpAgentMod::slowdown(int how){
+    // printf("from slowdown\n");
+
+	double decrease;  /* added for highspeed - sylvia */
+	double win, halfwin, decreasewin;
+	int slowstart = 0;
+	++ncwndcuts_;
+	if (!(how & TCP_IDLE) && !(how & NO_OUTSTANDING_DATA)){
+		++ncwndcuts1_; 
+	}
+	// we are in slowstart for sure if cwnd < ssthresh
+	if (cwnd_ < ssthresh_) 
+		slowstart = 1;
+        if (precision_reduce_) {
+		halfwin = windowd() / 2;
+                if (wnd_option_ == 6) {         
+                        /* binomial controls */
+                        decreasewin = windowd() - (1.0-decrease_num_)*pow(windowd(),l_parameter_);
+                } else if (wnd_option_ == 8 && (cwnd_ > low_window_)) { 
+                        /* experimental highspeed TCP */
+			decrease = decrease_param();
+			//if (decrease < 0.1) 
+			//	decrease = 0.1;
+			decrease_num_ = decrease;
+                        decreasewin = windowd() - (decrease * windowd());
+                } else {
+	 		decreasewin = decrease_num_ * windowd();
+		}
+		win = windowd();
+	} else  {
+		int temp;
+		temp = (int)(window() / 2);
+		halfwin = (double) temp;
+                if (wnd_option_ == 6) {
+                        /* binomial controls */
+                        temp = (int)(window() - (1.0-decrease_num_)*pow(window(),l_parameter_));
+                } else if ((wnd_option_ == 8) && (cwnd_ > low_window_)) { 
+                        /* experimental highspeed TCP */
+			decrease = decrease_param();
+			//if (decrease < 0.1)
+                        //       decrease = 0.1;		
+			decrease_num_ = decrease;
+                        temp = (int)(windowd() - (decrease * windowd()));
+                } else {
+ 			temp = (int)(decrease_num_ * window());
+		}
+		decreasewin = (double) temp;
+		win = (double) window();
+	}
+	if (how & CLOSE_SSTHRESH_HALF)
+		// For the first decrease, decrease by half
+		// even for non-standard values of decrease_num_.
+		if (first_decrease_ == 1 || slowstart ||
+			last_cwnd_action_ == CWND_ACTION_TIMEOUT) {
+			// Do we really want halfwin instead of decreasewin
+		// after a timeout?
+			ssthresh_ = (int) halfwin;
+		} else {
+			ssthresh_ = (int) decreasewin;
+		}
+        else if (how & THREE_QUARTER_SSTHRESH)
+		if (ssthresh_ < 3*cwnd_/4)
+			ssthresh_  = (int)(3*cwnd_/4);
+	if (how & CLOSE_CWND_HALF)
+		// For the first decrease, decrease by half
+		// even for non-standard values of decrease_num_.
+		if (first_decrease_ == 1 || slowstart || decrease_num_ == 0.5) {
+			// printf("changed stuff \n");
+			cwnd_ = (halfwin * 2) * (3. * N - 1) / (3. * N + 1);
+		} else cwnd_ = decreasewin;
+        else if (how & CWND_HALF_WITH_MIN) {
+		// We have not thought about how non-standard TCPs, with
+		// non-standard values of decrease_num_, should respond
+		// after quiescent periods.
+                cwnd_ = decreasewin;
+                if (cwnd_ < 1)
+                        cwnd_ = 1;
+	}
+	else if (how & CLOSE_CWND_RESTART) 
+		cwnd_ = int(wnd_restart_);
+	else if (how & CLOSE_CWND_INIT)
+		cwnd_ = int(wnd_init_);
+	else if (how & CLOSE_CWND_ONE)
+		cwnd_ = 1;
+	else if (how & CLOSE_CWND_HALF_WAY) {
+		// cwnd_ = win - (win - W_used)/2 ;
+		cwnd_ = W_used + decrease_num_ * (win - W_used);
+                if (cwnd_ < 1)
+                        cwnd_ = 1;
+	}
+	if (ssthresh_ < 2)
+		ssthresh_ = 2;
+	if (how & (CLOSE_CWND_HALF|CLOSE_CWND_RESTART|CLOSE_CWND_INIT|CLOSE_CWND_ONE))
+		cong_action_ = TRUE;
+
+	fcnt_ = count_ = 0;
+	if (first_decrease_ == 1)
+		first_decrease_ = 0;
+	// for event tracing slow start
+	if (cwnd_ == 1 || slowstart) 
+		// Not sure if this is best way to capture slow_start
+		// This is probably tracing a superset of slowdowns of
+		// which all may not be slow_start's --Padma, 07/'01.
+		trace_event("SLOW_START");
+	
+
+
+
+    // RenoTcpAgent::slowdown(how);
+}
+
+
+
+void TcpAgentMod::rtt_update(double tao){
+    // printf("from rtt\n");
+	double now = Scheduler::instance().clock();
+	if (ts_option_)
+		t_rtt_ = int(tao /tcp_tick_ + 0.5);
+	else {
+		double sendtime = now - tao;
+		sendtime += boot_time_;
+		double tickoff = fmod(sendtime, tcp_tick_);
+		t_rtt_ = int((tao + tickoff) / tcp_tick_);
+	}
+	if (t_rtt_ < 1)
+		t_rtt_ = 1;
+	//
+	// t_srtt_ has 3 bits to the right of the binary point
+	// t_rttvar_ has 2
+        // Thus "t_srtt_ >> T_SRTT_BITS" is the actual srtt, 
+  	//   and "t_srtt_" is 8*srtt.
+	// Similarly, "t_rttvar_ >> T_RTTVAR_BITS" is the actual rttvar,
+	//   and "t_rttvar_" is 4*rttvar.
+	//
+        if (t_srtt_ != 0) {
+		register short delta;
+		delta = t_rtt_ - (t_srtt_ >> T_SRTT_BITS);	// d = (m - a0)
+		if ((t_srtt_ += delta) <= 0)	// a1 = 7/8 a0 + 1/8 m
+			t_srtt_ = 1;
+		if (delta < 0)
+			delta = -delta;
+		delta -= (t_rttvar_ >> T_RTTVAR_BITS);
+		if ((t_rttvar_ += delta) <= 0)	// var1 = 3/4 var0 + 1/4 |d|
+			t_rttvar_ = 1;
+	} else {
+		t_srtt_ = t_rtt_ << T_SRTT_BITS;		// srtt = rtt
+		t_rttvar_ = t_rtt_ << (T_RTTVAR_BITS-1);	// rttvar = rtt / 2
+	}
+	//
+	// Current retransmit value is 
+	//    (unscaled) smoothed round trip estimate
+	//    plus 2^rttvar_exp_ times (unscaled) rttvar. 
+	//
+	t_rtxcur_ = (((t_rttvar_ << (rttvar_exp_ + (T_SRTT_BITS - T_RTTVAR_BITS))) +
+		t_srtt_)  >> T_SRTT_BITS ) * tcp_tick_;
+
+    if (t_rtt_min > t_rtt_)
+        t_rtt_min = t_rtt_;
+
+	// printf("trtt:%d\n", t_rtt_.getValue());
+	rttSum += t_rtt_.getValue();
+	rttUpdateCount += 1;
+	return;
+
+}
+
+
+void TcpAgentMod::timeout(int tno)
+{
+    // printf("here\n");
+	/* retransmit timer */
+	if (tno == TCP_TIMER_RTX) {
+
+		// There has been a timeout - will trace this event
+		trace_event("TIMEOUT");
+
+		frto_ = 0;
+		// Set pipe_prev as per Eifel Response
+		pipe_prev_ = (window() > ssthresh_) ?
+			window() : (int)ssthresh_;
+
+	        if (cwnd_ < 1) cwnd_ = 1;
+		if (qs_approved_ == 1) qs_approved_ = 0;
+		if (highest_ack_ == maxseq_ && !slow_start_restart_) {
+			/*
+			 * TCP option:
+			 * If no outstanding data, then don't do anything.  
+			 */
+			 // Should this return be here?
+			 // What if CWND_ACTION_ECN and cwnd < 1?
+			 // return;
+		} else {
+			recover_ = maxseq_;
+			if (highest_ack_ == -1 && wnd_init_option_ == 2)
+				/* 
+				 * First packet dropped, so don't use larger
+				 * initial windows. 
+				 */
+				wnd_init_option_ = 1;
+                        else if ((highest_ack_ == -1) &&
+                                (wnd_init_option_ == 1) && (wnd_init_ > 1)
+				&& bugfix_ss_)
+                                /*
+                                 * First packet dropped, so don't use larger
+                                 * initial windows.  Bugfix from Mark Allman.
+                                 */
+                                wnd_init_ = 1;
+			if (highest_ack_ == maxseq_ && restart_bugfix_)
+			       /* 
+				* if there is no outstanding data, don't cut 
+				* down ssthresh_.
+				*/
+				slowdown(CLOSE_CWND_ONE|NO_OUTSTANDING_DATA);
+			else if (highest_ack_ < recover_ &&
+			  last_cwnd_action_ == CWND_ACTION_ECN) {
+			       /*
+				* if we are in recovery from a recent ECN,
+				* don't cut down ssthresh_.
+				*/
+				slowdown(CLOSE_CWND_ONE);
+				if (frto_enabled_ || sfrto_enabled_) {
+					frto_ = 1;
+				}
+			}
+			else {
+				++nrexmit_;
+				last_cwnd_action_ = CWND_ACTION_TIMEOUT;
+				slowdown(CLOSE_SSTHRESH_HALF|CLOSE_CWND_RESTART);
+				if (frto_enabled_ || sfrto_enabled_) {
+					frto_ = 1;
+				}
+			}
+		}
+		/* if there is no outstanding data, don't back off rtx timer */
+		if (highest_ack_ == maxseq_ && restart_bugfix_) {
+			reset_rtx_timer(0,0);
+		}
+		else {
+			reset_rtx_timer(0,1);
+		}
+		last_cwnd_action_ = CWND_ACTION_TIMEOUT;
+		send_much(0, TCP_REASON_TIMEOUT, maxburst_);
+	} 
+	else {
+		timeout_nonrtx(tno);
+	}
+
+	// if(t_srtt_ <= (1e-6))
+    //     return;
+    // double q = ((t_srtt_ - t_rtt_min) * awnd_) / t_srtt_;
+    // double r = (alpha * awnd_) / N;
+	if (windowUpdateCount == 0 || rttUpdateCount == 0)
+		return;
+	double avgwnd = windowSum * 1. / windowUpdateCount;
+	double avgRtt = rttSum * 1. / rttUpdateCount;
+    double q = ((avgRtt - t_rtt_min) * avgwnd) / avgRtt;
+    double r = (alpha * avgwnd) / N;
+    if(q < r){
+        N += 1;
+    }
+    else if(q == r){
+
+    }
+    else{
+        N = N > 2 ? N - 1 : 1;
+		printf("N: %d r:%lf q:%lf alpha:%lf t_rtt:%d t_rtt_min:%d srtt:%d avgwnd:%lf avgRtt:%lf\n", N,r,q,alpha, t_rtt_.getValue()
+		, t_rtt_min.getValue(), t_srtt_.getValue(), avgwnd, avgRtt);
+    }
+}
+
+
+void TcpAgentMod::recv(Packet *pkt, Handler*)
+{
+	hdr_tcp *tcph = hdr_tcp::access(pkt);
+	int valid_ack = 0;
+	if (qs_approved_ == 1 && tcph->seqno() > last_ack_) 
+		endQuickStart();
+	if (qs_requested_ == 1)
+		processQuickStart(pkt);
+#ifdef notdef
+	if (pkt->type_ != PT_ACK) {
+		Tcl::instance().evalf("%s error \"received non-ack\"",
+				      name());
+		Packet::free(pkt);
+		return;
+	}
+#endif
+	/* W.N.: check if this is from a previous incarnation */
+	if (tcph->ts() < lastreset_) {
+		// Remove packet and do nothing
+		Packet::free(pkt);
+		return;
+	}
+	++nackpack_;
+	ts_peer_ = tcph->ts();
+	int ecnecho = hdr_flags::access(pkt)->ecnecho();
+	if (ecnecho && ecn_)
+		ecn(tcph->seqno());
+	recv_helper(pkt);
+	recv_frto_helper(pkt);
+	/* grow cwnd and check if the connection is done */ 
+	if (tcph->seqno() > last_ack_) {
+		recv_newack_helper(pkt);
+		if (last_ack_ == 0 && delay_growth_) { 
+			cwnd_ = initial_window();
+		}
+	} else if (tcph->seqno() == last_ack_) {
+                if (hdr_flags::access(pkt)->eln_ && eln_) {
+                        tcp_eln(pkt);
+                        return;
+                }
+		if (++dupacks_ == numdupacks_ && !noFastRetrans_) {
+			dupack_action();
+		} else if (dupacks_ < numdupacks_ && singledup_ ) {
+			send_one();
+		}
+	}
+
+	if (QOption_ && EnblRTTCtr_)
+		process_qoption_after_ack (tcph->seqno());
+
+	if (tcph->seqno() >= last_ack_)  
+		// Check if ACK is valid.  Suggestion by Mark Allman. 
+		valid_ack = 1;
+	Packet::free(pkt);
+	/*
+	 * Try to send more data.
+	 */
+	if (valid_ack || aggressive_maxburst_)
+		send_much(0, 0, maxburst_);
+	// printf("recv: cwnd : %lf avgcwnd : %lf\n",cwnd_.getValue(),awnd_);
+	windowSum += cwnd_;
+	windowUpdateCount++; 
+}
